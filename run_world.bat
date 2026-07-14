b37fe27c1e5a8408c3174e756e0f61ed7f5183c9
k-edr <yevhenpuzenko@gmail.com>
2026-07-14 23:14:25 +0200

    Fix LangVersion to C# 9 and add auto-kill to run_world.bat
    - Bump LangVersion from 8 to 9 (needed for target-typed new())
    - Add taskkill before launch so -world flag works reliably

--- run_world.bat
+++ run_world.bat
@@ -1,4 +1,9 @@
 @echo off
+REM Kill any running SE instance first (required for -world to work)
+taskkill /f /im SpaceEngineers.exe >nul 2>&1
+taskkill /f /im SpaceEngineersLauncher.exe >nul 2>&1
+timeout /t 2 /nobreak >nul
+
 REM Launch Space Engineers directly into the specified world
 REM World: Empty World 2026-07-14 21:40
 start "" "D:\SteamLibrary\steamapps\common\SpaceEngineers\Bin64\SpaceEngineersLauncher.exe" -world "Empty World 2026-07-14 21-40"
