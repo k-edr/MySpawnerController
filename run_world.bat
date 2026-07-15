a84831d98f587035706161d41d6c7d56adb6ed06
k-edr <yevhenpuzenko@gmail.com>
2026-07-15 10:05:07 +0200

    Fix -world argument: remove quotes that SE parses literally

--- run_world.bat
+++ run_world.bat
@@ -5,6 +5,6 @@ taskkill /f /im SpaceEngineersLauncher.exe >nul 2>&1
 timeout /t 2 /nobreak >nul
 
 REM Launch Space Engineers directly into the specified world
-REM Using SpaceEngineers.exe (not launcher) for reliable -world argument passing
+REM NOTE: -world argument must NOT be quoted (SE parses it literally)
 cd /d D:\SteamLibrary\steamapps\common\SpaceEngineers\Bin64
-start SpaceEngineers.exe -world "Empty_World_In" -skipintro
+start SpaceEngineers.exe -world Empty_World_In -skipintro
